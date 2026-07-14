4afff327953dff8086fdd5dc023ba580b7ee469f
makeuscare <callum@makeus.care>
2026-07-14 18:20:43 +0100

    Header and additional style updates

--- wp-content/themes/seraphim-master/header.php
+++ wp-content/themes/seraphim-master/header.php
@@ -20,11 +20,15 @@
         <img src="https://investors.seraphim.vc/wp-content/themes/seraphimvc/src/images/logo.png" />
     </a>
  
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#mainNavbar" aria-controls="mainNavbar"
-            aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+	            data-bs-target="#mainNavbar" aria-controls="mainNavbar"
+	            aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon" aria-hidden="true">
+	        <span class="navbar-toggler-line"></span>
+	        <span class="navbar-toggler-line"></span>
+	        <span class="navbar-toggler-line"></span>
+	      </span>
+	    </button>
  
     <div class="collapse navbar-collapse" id="mainNavbar">
         
@@ -40,29 +44,29 @@
 
         <!-- Top-level dropdown -->
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-item" href="#" id="productsDropdown" role="button"
-             data-bs-toggle="dropdown" aria-expanded="false">
-            Investor Relations
-          </a>
+	          <a class="nav-link" href="#" id="productsDropdown" role="button"
+	             data-bs-toggle="dropdown" aria-expanded="false">
+	            Investor Relations
+	          </a>
           <ul class="dropdown-menu" aria-labelledby="productsDropdown">
-            <li><a class="dropdown-item" href="#">All Products</a></li>
+            <li><a class="dropdown-item" href="#">Item 1</a></li>
  
             <!-- Second-level dropdown (submenu) -->
             <li class="dropdown-submenu">
-              <a class="dropdown-item dropdown-item" href="#" id="electronicsDropdown">Electronics</a>
+	              <a class="dropdown-item submenu-item" href="#" id="electronicsDropdown">Item 2</a>
               <ul class="dropdown-menu" aria-labelledby="electronicsDropdown">
-                <li><a class="dropdown-item" href="#">Phones</a></li>
-                <li><a class="dropdown-item" href="#">Laptops</a></li>
-                <li><a class="dropdown-item" href="#">Accessories</a></li>
+                <li><a class="dropdown-item" href="#">One</a></li>
+                <li><a class="dropdown-item" href="#">Two</a></li>
+                <li><a class="dropdown-item" href="#">Three</a></li>
               </ul>
             </li>
  
             <li class="dropdown-submenu">
-              <a class="dropdown-item dropdown-item" href="#" id="clothingDropdown">Clothing</a>
+	              <a class="dropdown-item submenu-item" href="#" id="clothingDropdown">Item 3</a>
               <ul class="dropdown-menu" aria-labelledby="clothingDropdown">
-                <li><a class="dropdown-item" href="#">Men</a></li>
-                <li><a class="dropdown-item" href="#">Women</a></li>
-                <li><a class="dropdown-item" href="#">Kids</a></li>
+                <li><a class="dropdown-item" href="#">One</a></li>
+                <li><a class="dropdown-item" href="#">Two</a></li>
+                <li><a class="dropdown-item" href="#">Three</a></li>
               </ul>
             </li>
           </ul>
@@ -80,7 +84,7 @@
 
     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">SSIT</a>
+          <a class="nav-link" href="#">Why Space?</a>
         </li>
 
         <li class="nav-item">
@@ -91,6 +95,3 @@
     </div>
   </div>
 </nav>
-
-
-<main id="content" class="site-content">
